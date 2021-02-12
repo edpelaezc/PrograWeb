c0c6862b5cc079d82ea4896a9849ad6ca51caffd
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2021-02-11 22:21:40 -0600

    adding routing for login module

--- coffee/src/app/app.module.ts
+++ coffee/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginModule } from "./components/login/login.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutComponent } from './components/layout/layout.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
