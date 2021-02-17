58446ce85337befb9a6346b7c0d3f7e76b1d6b29
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2021-02-16 23:28:53 -0600

    using local storage for login

--- coffee/src/app/components/login/login/login.component.ts
+++ coffee/src/app/components/login/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginServiceService } from "../../services/login-service/login-service.service";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  public loginInvalid: boolean;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private loginService: LoginServiceService
+  ) { }
+
 
   ngOnInit(): void {
+    localStorage.setItem('user', 'eduanpelaezc@gmail.com');
+    localStorage.setItem('password', 'admin');
+
+    this.form = this.fb.group({
+      username: ['', Validators.email],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    let username= this.form.get('username')?.value;
+    let password = this.form.get('password')?.value;
+    if(this.loginService.signIn(username, password)) {
+      alert('¡Bienvenido!');
+    } else {
+      this.loginInvalid = true; 
+    };
   }
 
 }
--- coffee/src/app/components/services/login-service/login-service.service.ts
+++ coffee/src/app/components/services/login-service/login-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
 
-  constructor() { }
+  authSubject = new BehaviorSubject(false);
+
+  signIn(user: string, password: string) {
+    let userAux = localStorage.getItem('user')
+    let passwordAux = localStorage.getItem('password')
+
+    if (user == userAux && password == passwordAux) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isAuthenticated() {
+    return this.authSubject.asObservable();
+  }
 }
