cfeab8d75535e77e11e25e0c254e2c80718bf3f5
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2021-02-18 00:59:31 -0600

    AB#3 changes in routing

--- coffee/src/app/components/dashboard/dashboard-routing.module.ts
+++ coffee/src/app/components/dashboard/dashboard-routing.module.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from "./home/home.component";
 import { LayoutComponent } from './layout/layout.component';
 import { AuthGuard } from "../services/login-service/auth/auth.guard";
 import { UserComponent } from './user/user.component';
+import { HistoryComponent } from './history/history.component';
+import { MethodsComponent } from './methods/methods.component';
+import { ProcessComponent } from './process/process.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent,
   children: [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'MiCuenta', component: UserComponent, canActivate: [AuthGuard]}
+    { path: 'MiCuenta', component: UserComponent, canActivate: [AuthGuard] },
+    { path: 'Historia', component: HistoryComponent, canActivate: [AuthGuard] },
+    { path: 'Metodos', component: MethodsComponent, canActivate: [AuthGuard] },
+    { path: 'Proceso', component: ProcessComponent, canActivate: [AuthGuard] }
   ], canActivate: [AuthGuard] }
 ];
 
