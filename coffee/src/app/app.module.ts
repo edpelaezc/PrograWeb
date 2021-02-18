c92e1b3753f7215b12868438a00c2ca24fabc48b
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2021-02-17 22:35:10 -0600

    Adding Dashboard to routing AB#3

--- coffee/src/app/app.module.ts
+++ coffee/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material/material.module";
 import { FormsModule } from '@angular/forms';
+import { DashboardRoutingModule } from "./components/dashboard/dashboard-routing.module";
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     LoginModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    DashboardRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
